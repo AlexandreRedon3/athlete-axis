617fcdbdf764b15ded3fb59c47c3fc17602dc7c7
WasthElse2002 <alexandre.redon@ynov.com>
2025-08-19 17:00:59 +0200

    fix: .env

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-// app/page.tsx
-import LandingPage from "./(landing)/page";
+// app/page.ts
+"use client";
+import { UserLanding } from "../src/components/landing-page/user-landing";
 
 export default async function Page() {
-  return <LandingPage />;
+  return <UserLanding />;
 }
